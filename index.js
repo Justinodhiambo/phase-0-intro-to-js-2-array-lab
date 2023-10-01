3e13555372d78cfa6a1e7fc84f6566b62789ca19
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-10-01 05:09:30 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+  function removeFirstCat() {
+    if (cats.length > 0) {
+      return cats.slice(1);
+    } else {
+      return [];
+    }
+  }
+
+  function appendCat(name) {
+    return [...cats, name];
+}
+
+
